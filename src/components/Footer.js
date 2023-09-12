afb8ba8f9140ad88ad8f16cb2410f1b36d46337d
Stas <stas34339@mail.ru>
2023-09-12 22:58:57 +0300

    Created header main footer

---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+import './Footer.css'
+function Footer()
+{
+    return(
+        <footer>
+            <div className='container'>
+                <div className='footer-block'>
+                    <div className='tale'>
+                        tale
+                    </div>
+                    <div className='user'>
+                        kirill borisyonok
+                    </div>
+                    <div className='email'>
+                        <a href='#'>boravdv@mail.ru</a>
+                    </div>
+                    <div className='date'>
+                        18 Feb 2022
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
+export default Footer
