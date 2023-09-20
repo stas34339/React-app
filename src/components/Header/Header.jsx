bb3a902c9fff388875ed820ee40cadab458fc2a4
Stas <stas34339@mail.ru>
2023-09-20 03:43:37 +0300

    Used scss in Haader Main Footer

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,41 +1,40 @@
 import { useState } from 'react'
-import './Header.css'
+import './Header.scss'
+import './Header_extension.scss'
 import { BiDoorOpen } from 'react-icons/bi'
 
 function Header() {
     return (
-      <header>
-        <div className='container'>
-            <div className='header-block'>
-                <div className='nav'>
-                    <img src='/Subtract.png'></img>
-                    <ul className='breadcrumb'>
-                        <li class="breadcrumb-home"><a href="#">home</a></li>
-                        <li class="breadcrumb-tweet"><a href="#">tweet</a></li>
-                        <li class="breadcrumb-login"><a href="#">log in</a></li>
-                        <li class="breadcrumb-signup"><a href="#">sign up</a></li>
-                        <li class="breadcrumb-error"><a href="#">error</a></li>
-                    </ul>
-                </div>
-                <div className="header-login">
-                    <button className='login'>
-                        log in
-                    </button>
-                </div>
-                <div className="header-logout">
-                    <div className="username">
-                        <div className="avatar">
+      <header className='header'>
+        <div className='header__container'>
+            <div className='header__nav'>
+                <a href="#" className='header__logo'><img src='/Subtract.png'></img></a>
+                <ul className='header__breadcrumb'>
+                    <li className="header-item header-item-home header-item-active"><a href="#">home</a></li>
+                    <li className="header-item header-item-tweet"><a href="#">tweet</a></li>
+                    <li className="header-item header-item-login"><a href="#">log in</a></li>
+                    <li className="header-item header-item-signup"><a href="#">sign up</a></li>
+                    <li className="header-item header-item-error"><a href="#">error</a></li>
+                </ul>
+            </div>
+            <div className="header__login">
+                <button className='header__btn'>
+                    <p>log in</p>
+                </button>
+            </div>
+            <div className="header__logout">
+                <div className="header__username">
+                    <div className="header__avatar">
 
-                        </div>
-                        <p className='name'>
-                            Kirill Borisyonok
-                        </p>
                     </div>
-                    <button className='logout'>
-                        <BiDoorOpen />
-                        <p>LOG OUT</p>
-                    </button>
+                    <p className='header__name'>
+                        Kirill Borisyonok
+                    </p>
                 </div>
+                <button className='header__btn'>
+                    <BiDoorOpen className='btn-item'/>
+                    <p>log out</p>
+                </button>
             </div>
         </div>
       </header>
